4b599792c4730cb12a7303e1d146a169857d99ca
Brock Williams <b.williams54255@gmail.com>
2024-01-08 18:55:36 -0800

    Intalized client directory and created header.jsx and css.

---
+++ recipe/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+   
+    const isAuthenticated = false; 
+
+    return (
+        <header className="header">
+            <div className="logo">
+                <Link to="/">Spoons</Link>
+            </div>
+            <nav className="navigation">
+                <Link to="/">Home</Link>
+                <Link to="/recipes">Browse Recipes</Link>
+                {isAuthenticated ? (
+                    <>
+                        <Link to="/create">Create Recipe</Link>
+                        <Link to="/profile">Profile</Link>
+                        <Link to="/logout">Log Out</Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">Log In</Link>
+                        <Link to="/signup">Sign Up</Link>
+                    </>
+                )}
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
