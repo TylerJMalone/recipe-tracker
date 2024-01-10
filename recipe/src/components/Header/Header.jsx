d441fd5d351024ac4ed52730914041a276dfc968
Brock Williams <b.williams54255@gmail.com>
2024-01-09 20:07:22 -0800

    Created RecipeDetailsPage.jsx for single recipes. Added basic styling.

--- recipe/src/components/Header/Header.jsx
+++ recipe/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import './winter.css';
+
 
 function Header() {
    
--- recipe/src/pages/SearchRecipesPage/SearchRecipesPage.jsx
+++ recipe/src/pages/SearchRecipesPage/SearchRecipesPage.jsx
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SearchRecipesPage.css';
 
 function SearchRecipesPage() {
     const [searchQuery, setSearchQuery] = useState('');
-    const [recipes, setRecipes] = useState([]); // State to hold search results
-    const [isLoading, setIsLoading] = useState(false); 
+    const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSearch = async (e) => {
         e.preventDefault();
         setIsLoading(true);
 
         try {
-           
             const response = await fetch(`http://localhost:5000/api/searchRecipes?query=${encodeURIComponent(searchQuery)}`);
-            
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-
             const data = await response.json();
             setRecipes(data.results);
         } catch (error) {
             console.error("Failed to fetch recipes:", error);
-           
         } finally {
             setIsLoading(false);
         }
@@ -41,16 +38,13 @@ function SearchRecipesPage() {
                     <button type="submit" disabled={isLoading}>Search</button>
                 </form>
             </div>
-            <div className="filter-options">
-                {/* Filter options here */}
-            </div>
             <div className="search-results">
                 {isLoading ? <p>Loading...</p> : 
                     recipes.map(recipe => (
-                        <div key={recipe.id}>
+                        <div key={recipe.id} className="recipe-item">
                             <h3>{recipe.title}</h3>
                             <img src={recipe.image} alt={recipe.title} />
-                            {/* Additional recipe details */}
+                            <Link to={`/recipe/${recipe.id}`}>View Details</Link> {/* Link to Recipe Details */}
                         </div>
                     ))
                 }
